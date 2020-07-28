3550441c7d650eb838db50f80176702f15ca1b33
slakedavid2 <45336986+slakedavid2@users.noreply.github.com>
2020-07-27 21:43:36 -0400

    modified:   crowbot.py

--- crowbot.py
+++ crowbot.py
@@ -11,44 +11,6 @@ translator = Translator()
 #===============================================================================================#
 @client.event
 async def on_message(message):
-    if (message.author == client.user):
-        return
-    #===========================================================================================#
-    if "crowbot" in message.content.lower() or client.user.mentioned_in(message):
-        i = randint(1, 14)
-        send_message_choice = send_message[str(i)]
-        await message.channel.send(send_message_choice)
-    #===========================================================================================#
-    authenticator = IAMAuthenticator('<api-key>')
-    language_translator = LanguageTranslatorV3(
-    version='<version>',
-    authenticator=authenticator)
-    language_translator.set_service_url('<url>')
-    #===========================================================================================#
-    t=str(message.content)
-    x=re.findall(r'<.*:.*:.*>',t)
-    if(len(x)>0):
-        for i in x: t.replace(i,"|reaction|")
-    l = translator.detect(t)
-    if l.confidence < 0.7 or 'en' in str(l.lang):
-        return
-    l=l.lang
-    #===========================================================================================#
-    print(l)
-    try:
-        translation = language_translator.translate(text=t,model_id=l+'-en').get_result()
-        tr=(dict(json.loads(json.dumps(translation, indent=2, ensure_ascii=False)).items())['translations'][0]['translation'])
-    except Exception as e:
-        print(str(e))
-        tr=translator.translate(t).text
-    if(str(tr)==str(message.content)):
-        return
-    print(tr)
-    await message.channel.send("Translation : " + tr)
-
-@client.event
-async def on_ready():
-    print("Logged In")
     send_message = {
     "1": "fuck off",
     "2": "CAW CAW, MOTHA FUCKA",
@@ -64,5 +26,45 @@ async def on_ready():
     "12": "I do my best.",
     "13": "I am programmed to kill, you know.",
     "14": "I like failbot."}
+    if (message.author == client.user):
+        return
+    #===========================================================================================#
+    if "crowbot" in message.content.lower() or client.user.mentioned_in(message):
+        i = randint(1, 14)
+        send_message_choice = send_message[str(i)]
+        await message.channel.send(send_message_choice)
+    #===========================================================================================#
+    authenticator = IAMAuthenticator('<api key>')
+    language_translator = LanguageTranslatorV3(
+    version='<version>',
+    authenticator=authenticator)
+    language_translator.set_service_url('<url>')
+    #===========================================================================================#
+    t=str(message.content)
+    print(t)
+    x=re.findall(r'<.*:.*:.*>',t)
+    if(len(x)>0):
+        for i in x: t.replace(i,"|reaction|")
+    l = translator.detect(t)
+    if l.confidence < 0.7 or 'en' in str(l.lang):
+        return
+    l=l.lang
+    #===========================================================================================#
+    print(l)
+    try:
+        translation = language_translator.translate(text=t,model_id=l+'-en').get_result()
+        tr=(dict(json.loads(json.dumps(translation, indent=2, ensure_ascii=False)).items())['translations'][0]['translation'])
+    except Exception as e:
+        print(str(e))
+        tr=translator.translate(t).text
+        pass
+    print(tr)
+    if(str(tr).lower().strip()==str(message.content.lower().strip())):
+        return
+    await message.channel.send("Translation : " + tr)
 
+@client.event
+async def on_ready():
+    print("Logged In")
+    
 client.run('<bot token>',bot=True)
